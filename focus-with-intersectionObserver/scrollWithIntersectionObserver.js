aa9d3eccf57ce885cea1a4da4d3eff25dbc762de
Jihye Hong <jh.hong@lge.com>
2017-10-18 20:43:13 +0900

    Update scrollWithIntersectionObserver.js

--- focus-with-intersectionObserver/scrollWithIntersectionObserver.js
+++ focus-with-intersectionObserver/scrollWithIntersectionObserver.js
@@ -128,10 +128,10 @@ function handler(entries, observer) {
      document.getElementById('statusText').textContent = ' entirely in the view ';
     } else if (intersectionRatio <= 0) {
       statusBox.className = 'no';
-      document.getElementById('statusText').textContent = ' entirely out of the view ';
+      document.getElementById('statusText').textContent = ' entirely hidden ';
     } else {
       statusBox.className = 'partial';
-      document.getElementById('statusText').textContent = ' partialy in the view ';
+      document.getElementById('statusText').textContent = ' partialy hidden ';
     }
   }
 }
