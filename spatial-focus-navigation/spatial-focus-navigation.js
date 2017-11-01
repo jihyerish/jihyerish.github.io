f9d306ffd8bb2ec7bfad63334046b8a78a2dbd30
Jihye Hong <jh.hong@lge.com>
2017-11-01 20:21:35 +0900

    Update some fuctions

--- spatial-focus-navigation/spatial-focus-navigation.js
+++ spatial-focus-navigation/spatial-focus-navigation.js
@@ -1,6 +1,11 @@
 var boxes;
-var focusedElement;
+var focusedElement = {
+  box: null,
+  position: null
+};
+
 var pressedKey = false;
+var targetElement;
 
 var keyCodes = {
   9 : "tab",
@@ -9,6 +14,41 @@ var keyCodes = {
   39 : "right-arrow",
   40 : "down-arrow"
 };
+
+//var day = {"sun", "mon", "tue", "wed", "thu", "fri", "sat"};
+
+/*
+var focusedElementInfo = {
+  box: focusedElement,
+  position: getPosition(focusedElement)
+};
+*/
+
+function getPosition(el) {
+  var xPos = 0;
+  var yPos = 0;
+
+  while (el) {
+    if (el.tagName == "BODY") {
+      // deal with browser quirks with body/window/document and page scroll
+      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+      var yScroll = el.scrollTop || document.documentElement.scrollTop;
+
+      xPos += (el.offsetLeft - xScroll + el.clientLeft);
+      yPos += (el.offsetTop - yScroll + el.clientTop);
+    } else {
+      // for all other non-BODY elements
+      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+    }
+
+    el = el.offsetParent;
+  }
+  return {
+    x: xPos,
+    y: yPos
+  };
+}
 
 function getNextFocusableElement(direction){
   if(direction == 37) {
@@ -20,25 +60,69 @@ function getNextFocusableElement(direction){
   } else if(direction == 40) {
     getDownElement()
   }
+
+  //console.log("x: "+focusedElement.position.x+", y: "+focusedElement.position.y);
 }
 
 function getUpElement(){
-
   console.log("Up");
+  //if Nearest Element
+
+  //else if Projection Element
+
+
+  //else if Direction Element
+
 }
 
 function getDownElement(){
   console.log("Down");
+
+  //if Nearest Element
+
+  //else if Projection Element
+
+  //else if Direction Element
 }
 
 function getLeftElement(){
-
   console.log("Left");
+
+  //if Nearest Element
+
+  //else if Projection Element
+
+  //else if Direction Element
 }
 
 function getRightElement(){
   console.log("Right");
 
+  //if Nearest Element
+
+  //else if Projection Element
+
+  //else if Direction Element
+
+}
+
+/*
+
+function getNearestElement(){
+
+}
+
+function getProjectionElement(){
+
+}
+
+function getDirectionElement(){
+
+}
+*/
+
+function setFocus(){
+  targetElement.focus();
 }
 
 function getPressedKey(){
@@ -55,13 +139,14 @@ function getPressedKey(){
 function init() {
   boxes = new Array();
 
-  focusedElement = document.activeElement;
+  document.querySelector('body').onfocus = function (e) {
+    focusedElement.box = document.activeElement;
+    focusedElement.position = getPosition(focusedElement.box);
+
+    console.log();
+  };
 
   getPressedKey();
-
-  //if(!pressedKey){
-
-  //}
 }
 
 window.onload = init;
