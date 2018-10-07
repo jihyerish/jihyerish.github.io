5782a385a7320e77e4e1cf50179ec75859b8aec9
Jihye Hong <jihyerish@gmail.com>
2018-10-07 12:10:07 +0900

    Follow the guideline

--- demos/observers-sample/intersection-observer/simpleIntersectionObserver.js
+++ demos/observers-sample/intersection-observer/simpleIntersectionObserver.js
@@ -64,41 +64,7 @@ const init = () => {
 			return index * 0.01;
 		})
 	});
-
-	
 }
-
-
-
-/* This custom threshold invokes the handler whenever:
-	   1. The target begins entering the viewport (0 < ratio < 1).
-	   2. The target fully enters the viewport (ratio >= 1).
-	   3. The target begins leaving the viewport (1 > ratio > 0).
-	   4. The target fully leaves the viewport (ratio <= 0).
-  
-	observer = new IntersectionObserver(handler, {
-	  threshold: [0, 1]
-	});
-
-
-function handler(entries, observer) {
-  for (entry of entries) {
-    let intersectionRatio = entry.intersectionRatio;
-
-    document.getElementById('ratio').textContent = intersectionRatio;
-
-    if (intersectionRatio >= 1) {
-     statusBox.className = 'yes';
-     document.getElementById('statusText').textContent = ' entirely in the view ';
-    } else if (intersectionRatio <= 0) {
-      statusBox.className = 'no';
-      document.getElementById('statusText').textContent = ' entirely hidden ';
-    } else {
-      statusBox.className = 'partial';
-      document.getElementById('statusText').textContent = ' partialy hidden ';
-    }
-  }
-} */
 
 window.addEventListener("load", function() {
   init();
