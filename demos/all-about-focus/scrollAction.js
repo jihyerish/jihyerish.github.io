9f4e5a64ca4088065797fc1feafaf24f5e19cfbf
Jihye Hong <jh.hong@lge.com>
2019-04-11 17:59:55 +0900

    Fix focus demo and script

--- demos/all-about-focus/scrollAction.js
+++ demos/all-about-focus/scrollAction.js
@@ -1,108 +1,109 @@
-var scrollBox;
-var boxes;
-var selectedBox;
+/* eslint-disable no-tabs */
+(function () {
 
-// number of boxes
-var numBoxes = 18;
-var method = 'focus';
-var display = 'block';
-var inlineValue = 'nearest';
-var blockValue = 'start';
-var behavior;
+	let scrollBox;
+	let boxes;
+	let selectedBox;
 
-document.getElementById('displayType').addEventListener('change', function(){
-	display = document.getElementById('displayType').options[document.getElementById('displayType').selectedIndex].value;
-	
-	if(display == 'block') {
-		document.getElementById('inlinePositionOptions').style.display='none';
-		document.getElementById('blockPositionOptions').style.display='inline';
+	// number of boxes
+	let numBoxes = 18;
+	let method = 'focus';
+	let inlineValue = 'nearest';
+	let blockValue = 'start';
+	let behavior;
+
+	const displayOption = document.getElementById('displayType');
+	const functionOption = document.getElementById('functionType');
+	const preventScroll = document.getElementById('preventScroll');
+
+	if (displayOption) {
+		document.getElementById('displayType').addEventListener('change', () => {
+			if (displayOption && (displayOption.value === 'inline')) {
+				document.getElementById('inlinePositionOptions').style.display = 'inline';
+				document.getElementById('blockPositionOptions').style.display = 'none';
+
+				scrollBox.setAttribute('class', 'inline-scrollBox');
+					
+				for(let i = 0; i < boxes.length; i++) {
+					boxes[i].style.display = 'inline-block';
+					boxes[i].style.margin = 'auto 1em';
+				}
+				
+			}
+			else {				
+				document.getElementById('inlinePositionOptions').style.display = 'none';
+				document.getElementById('blockPositionOptions').style.display = 'inline';
+				
+				scrollBox.setAttribute('class', 'block-scrollBox');
+					
+				for(let i = 0; i < boxes.length; i++) {
+					boxes[i].style.display = 'block';
+					boxes[i].style.margin = '1em auto';
+				}				
+			}		
+		});
+	}
+
+	if (functionOption) {
+		functionOption.addEventListener('change', () => {
+			if(functionOption.value === 'focus') {
+				document.getElementById('scrollIntoViewOptions').style.display = 'none';
+			}
+			else {
+				document.getElementById('scrollIntoViewOptions').style.display = 'inline';
+			}
+		});
+	}
+
+	document.getElementById('manual').addEventListener('click', () => {
+		getOptions();
 		
-		scrollBox.setAttribute('class', 'block-scrollBox');
-		   
-		for(var i=0; i < boxes.length; i++){
-		  boxes[i].style.display='block';
-		  boxes[i].style.margin='1em auto';
+		if(method === 'scrollIntoView') {
+			document.getElementById(selectedBox).scrollIntoView({
+				behavior,
+				inline: inlineValue,
+				block: blockValue
+			});
 		}
-	}
-	else {
-	   document.getElementById('inlinePositionOptions').style.display='inline';
-	   document.getElementById('blockPositionOptions').style.display='none';
-	   
-	   scrollBox.setAttribute('class', 'inline-scrollBox');
-	   
-	   for(var i=0; i < boxes.length; i++){
-		   boxes[i].style.display='inline-block';
-		   boxes[i].style.margin='auto 1em';
-	   }
-	}
-});
+		else {
+			if (preventScroll.checked)
+				document.getElementById(selectedBox).focus({preventScroll: true});
+			else 
+				document.getElementById(selectedBox).focus({preventScroll: false});
+		}
+	});
 
-document.getElementById('functionType').addEventListener('change', function(){
-	method = document.getElementById('functionType').options[document.getElementById('functionType').selectedIndex].value;
+	function init() {
+		boxes = [];
+		scrollBox = document.querySelector('#scrollBox');
+		scrollBox.setAttribute('class', 'block-scrollBox');
+			
+		for(let i = 0; i < numBoxes; i++) {
+			boxes.push(document.createElement('div'));
+			boxes[i].appendChild(document.createTextNode(i + 1));
+			boxes[i].setAttribute('class', 'box');
+			boxes[i].setAttribute('id', i + 1);
+			boxes[i].setAttribute('tabindex', 1);
+			boxes[i].style.display = 'block';
+			boxes[i].style.margin = '1em auto';
+			scrollBox.appendChild(boxes[i]);
+		}
 	
-	if(method == 'focus') {
-		document.getElementById('scrollIntoViewOptions').style.display='none';
-	}
-	else {
-      document.getElementById('scrollIntoViewOptions').style.display='inline';
-    }
-});
-
-document.getElementById('manual').addEventListener('click', function() {
-	getOptions();
-	
-  	if(method == 'scrollIntoView'){
-		document.getElementById(selectedBox).scrollIntoView({
-        behavior: behavior,
-        inline: inlineValue,
-        block: blockValue
-      });
-	}
-	else {
-		document.getElementById(selectedBox).focus();
-	}
-});
-
-function init() {
-	boxes = new Array();
-	scrollBox = document.querySelector('#scrollBox');
-	scrollBox.setAttribute('class', 'block-scrollBox');
-		
-	for(var i=0; i < numBoxes; i++){
-		boxes.push(document.createElement('div'));
-		boxes[i].appendChild(document.createTextNode(i+1));
-		boxes[i].setAttribute('class', 'box');
-		boxes[i].setAttribute('id', i+1);
-		boxes[i].setAttribute('tabindex', 1);
-		boxes[i].style.display='block';
-		scrollBox.appendChild(boxes[i]);
-	}
-	
-	console.log('box num: '+boxes.length);
-	
-	getOptions();
-}
-
-function getOptions() {
-	method = document.getElementById('functionType').options[document.getElementById('functionType').selectedIndex].value;
-	display = document.getElementById('displayType').options[document.getElementById('displayType').selectedIndex].value;
-	
-	console.log('display: '+ display + ', fuction: ' + method);
-	
-	if(method == 'scrollIntoView'){
-		behavior = document.getElementById('behavior').options[document.getElementById('behavior').selectedIndex].value;
-		inlineValue = document.getElementById('inlinePosition').options[document.getElementById('inlinePosition').selectedIndex].value;
-		blockValue = document.getElementById('blockPosition').options[document.getElementById('blockPosition').selectedIndex].value;
-
-		console.log('block: '+ blockValue + ', inline: '+ inlineValue);
-	}
-	else {
-		console.log('block: '+ blockValue + ', inline: '+ inlineValue);
+		getOptions();
 	}
 
-	selectedBox = document.getElementById('selectElement').options[document.getElementById('selectElement').selectedIndex].value;
-	
-	console.log('Id of the selected element: '+ selectedBox);
-}
+	function getOptions() {
+		if (functionOption)
+			method = document.getElementById('functionType').value;
 
-window.onload = init;
+		if(method === 'scrollIntoView') {
+			behavior = document.getElementById('behavior').value;
+			inlineValue = document.getElementById('inlinePosition').value;
+			blockValue = document.getElementById('blockPosition').value;
+		}
+		selectedBox = document.getElementById('selectElement').value;
+	}
+
+	window.addEventListener('load', () => {	init(); });
+
+})();
